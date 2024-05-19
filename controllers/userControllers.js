8b73e307ce66506b1dfee8d97175bb2a2e6ff41c
Jaymin Bhavsar <jayminbhavsar07@gmail.com>
2024-05-19 15:14:29 +0530

    Updated userControllers.js; modified getUsers

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,5 +1,12 @@
+const User = require("../models/userModel");
+
 function getUsers(req, res) {
-  res.end("Users!");
+  User.find()
+    .then((result) => res.json)
+    .catch((error) => {
+      console.error(error);
+      res.end("Error occured while fetching the users :(");
+    });
 }
 
 module.exports = { getUsers };
